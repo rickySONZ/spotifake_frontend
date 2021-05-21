8f68003702bb710f3e05f955ed315c466cf8e6fb
Ryan Erricson <ryanerricson@gmail.com>
2021-05-21 16:25:27 -0400

    Can successfully fetch library from rails API

--- src/libraryHomePage.js
+++ src/libraryHomePage.js
@@ -2,4 +2,11 @@ function appendLibraryAfterLogin(){
     let libraryDiv = document.createElement('div')
     libraryDiv.classList = 'library-div'
     document.body.append(libraryDiv)
+
+    let sessionID = sessionStorage.userID
+
+    fetch(`http://localhost:3000/users/${sessionID}/libraries/1`)
+    .then(res => console.log(res.json()))
+  
 }
+
