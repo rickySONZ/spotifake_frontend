8bb5fc40c20083b6d7acba911a37f585053a061a
Ryan Erricson <ryanerricson@gmail.com>
2021-05-26 11:12:08 -0400

    Modal shows song name, album, artist

--- src/song.js
+++ src/song.js
@@ -11,19 +11,9 @@ class Song {
     addSearchedSong(){
         let newLi = document.createElement('li')
         newLi.id = `search-song-${this.id}`
-        newLi.innerText = this.name
+        newLi.innerText = `${this.name} - ${this.album} - ${this.artist}`
         const modalList = document.querySelector('.search-list')
         modalList.append(newLi)
-    //     console.log(this)
-    //     newLi.id = `liked-song-${this.id}`
-    //     let deleteButton = document.createElement('button')
-    //     deleteButton.innerText = 'X'
-    //     deleteButton.setAttribute('data-action', 'delete')
-    //     deleteButton.classList.add('button')
-    //     deleteButton.addEventListener("click", this.deleteLikedSong.bind(this))
-    //     newLi.innerHTML = `${this.name} - ${this.album} - ${this.artist}`
-    //     newLi.appendChild(deleteButton)
-    //    const libraryList = document.getElementsByClassName('library-list')[0]
-    //    libraryList.append(newLi)
+        
     }
 }
